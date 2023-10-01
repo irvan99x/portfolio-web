6f3f549133d2ce2fa2eae1710de851cb651c396b
panzz09 <irvanirvan@gmail.com>
2023-10-01 08:58:23 +0700

    change some photo and text

--- src/components/certificate/Data.jsx
+++ src/components/certificate/Data.jsx
@@ -1,41 +1,33 @@
-import Serti1 from "../../assets/sertiteknisi.png";
-import Serti2 from "../../assets/toefl1.png";
-import Serti3 from "../../assets/toefl2.png";
-import Serti4 from "../../assets/teknisikomputer.png";
-import Serti5 from "../../assets/udemy1.png";
-import Serti6 from "../../assets/dicoding2.png";
+import Serti1 from "../../assets/toefl2.png";
+import Serti2 from "../../assets/teknisikomputer.png";
+import Serti3 from "../../assets/backend-expert.png";
+import Serti4 from "../../assets/udemy1.png";
+import Serti5 from "../../assets/aws.png";
 
 export const Data = [
   {
     id: 1,
-    image: Serti3,
-    title: "Test Of English as a Foreign Language - IES Foundation",
+    image: Serti1,
+    title: "TOEFL ITP - IES Foundation",
   },
   {
     id: 2,
-
     image: Serti2,
-    title: "Test Of English as a Foreign Language - IES Foundation",
+    title: "Supervisor Teknisi Komputer - BNSP",
   },
   {
     id: 3,
-    image: Serti4,
-    title: "Supervisor Teknisi Komputer - BNSP",
+    image: Serti3,
+    title: "Menjadi Back-End Developer Expert - Dicoding",
   },
   {
     id: 4,
-    image: Serti1,
-    title: "Supervisor Teknisi Komputer - BNSP",
-  },
-  {
-    id: 5,
-    image: Serti6,
-    title: "Membuat Aplikasi Web dengan React - Dicoding",
-  },
-  {
-    id: 6,
-
-    image: Serti5,
+    image: Serti4,
     title: "Full Stack Mobile Apps Development - Udemy",
+  },
+  {
+    id: 4,
+    image: Serti5,
+    title: "Architecture on AWS - Dicoding",
   },
 ];
